73ad5fd62df7b1a8dc2a925255023223c64a03f8
Adam Basha <110662505+Bashamega@users.noreply.github.com>
2025-01-01 10:48:56 +0200

    Update feature-box.php

--- templates/feature-box.php
+++ templates/feature-box.php
@@ -2,15 +2,17 @@
 if (!defined('ABSPATH')) exit;
 ?>
 <div class="wow zoomIn" style="visibility: visible; animation-name: zoomIn;">
+
     <div class="feature-box mb-md-3">
-        <?php \Elementor\Icons_Manager::render_icon($settings['icon'], ['aria-hidden' => 'true']); ?>
-        <h4>
-            <a href="<?php echo esc_url($settings['link']['url']); ?>"
-               <?php echo $settings['link']['is_external'] ? 'target="_blank"' : ''; ?>
+		<a href="<?php echo esc_url($settings['link']['url']); ?>"                <?php echo $settings['link']['is_external'] ? 'target="_blank"' : ''; ?>
                <?php echo $settings['link']['nofollow'] ? 'rel="nofollow"' : ''; ?>>
+            <?php \Elementor\Icons_Manager::render_icon($settings['icon'], ['aria-hidden' => 'true']); ?>
+            <h4>
+
                 <?php echo esc_html($settings['title']); ?>
-            </a>
-        </h4>
-        <p><?php echo esc_html($settings['description']); ?></p>
+            </h4>
+            <p><?php echo esc_html($settings['description']); ?></p>
+		</a>
+
     </div>
 </div>
